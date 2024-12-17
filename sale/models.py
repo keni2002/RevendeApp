e7bb382a7be60716ee81f0277ae9b85484b02e26
keni2002 <yerbisuniverses@gmail.com>
2024-12-17 16:46:11 -0500

    + primer modelo de Sale

--- sale/models.py
+++ sale/models.py
@@ -1,3 +1,30 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from customer.models import Customer
+from product.models import Product
+
+class Sale(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sales')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='sales')
+    products = models.ManyToManyField(Product, through='SaleProduct')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_profit = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Sale to {self.customer.name} on {self.created}"
+
+class SaleProduct(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    @property
+    def total_profit(self):
+        return (self.product.high_price - self.product.price) * self.quantity
